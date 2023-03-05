29d05f99fa7ee5eb06e6437eb962b8c6ef99cad3
purupuruputtyan <ec2-user@ip-172-31-7-79.ap-northeast-1.compute.internal>
2023-03-05 07:22:14 +0000

    In the making

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,5 @@
 class BooksController < ApplicationController
   def create
-    @book = Book.new
     book = Book.new(book_params)
     book.save
     redirect_to '/books'
@@ -8,6 +7,7 @@ class BooksController < ApplicationController
   
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
