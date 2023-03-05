30b610ddd5221935653cf678a60f091ec89b566d
purupuruputtyan <ec2-user@ip-172-31-7-79.ap-northeast-1.compute.internal>
2023-03-05 10:38:24 +0000

    In the making

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
     book.save
     redirect_to '/books'
   end
-  
+
   def index
     @books = Book.all
     @book = Book.new
@@ -17,19 +17,19 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    @book = Book.find(params[:id])
+    book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)  
+    redirect_to book_path(book.id)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to '/books'
   end
-  
+
   private
   # ストロングパラメータ
   def book_params
