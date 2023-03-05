d3040bc54e67e4ed350cd6f7e2cc8b5e76bee67e
purupuruputtyan <ec2-user@ip-172-31-7-79.ap-northeast-1.compute.internal>
2023-03-05 14:04:24 +0000

    In the making

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @books = Book.all
     if @book.save
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book.id)
     else
     render :index
     end
@@ -24,8 +24,12 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.new(book_params)
+    if @book.update(book_params)
     redirect_to book_path(book.id)
+    else
+    render :show
+    end
   end
 
   def destroy
