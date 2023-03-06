e4be9b6f870d8a4e91b96d0b188aa80afdc8279e
purupuruputtyan <ec2-user@ip-172-31-7-79.ap-northeast-1.compute.internal>
2023-03-06 09:20:48 +0000

    In the maing

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @books = Book.all
     if @book.save
+    flash[:notice] = 'Book was successfully created.'
     redirect_to book_path(@book.id)
     else
     render :index
@@ -25,6 +26,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+    flash[:notice] = 'Book was successfully updated.'
     redirect_to book_path(@book.id)
     else
     render :edit
@@ -34,6 +36,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = 'Book was successfully destroyed.'
     redirect_to books_path
   end
 
